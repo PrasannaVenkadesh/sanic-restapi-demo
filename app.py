c9982ab082b6f85902e4bb2ffe48bc6c86956505
Prasanna Venkadesh <prasanna.s@altencalsoftlabs.com>
2017-12-08 17:01:35 +0530

    returning with proper status codes

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 from sanic import Sanic
 from views.student import StudentsView, SingleStudentView
 
-app = Sanic()
 
 if __name__ == "__main__":
+    app = Sanic()
 
     # API routing for students
     app.add_route(StudentsView.as_view(), '/students')
-    app.add_route(SingleStudentView.as_view(), '/students/<id>')
+    app.add_route(SingleStudentView.as_view(), '/student/<id>')
 
     app.run(host='0.0.0.0', port=8000)
--- views/student.py
+++ views/student.py
@@ -1,6 +1,7 @@
 from sanic.response import json
 from sanic.views import HTTPMethodView
 import uuid
+
 # Key Value Store / In Memory DB
 in_memory_db = {}
 
@@ -23,23 +24,20 @@ def delete_kv(key):
     global in_memory_db
     if key in in_memory_db:
         in_memory_db[key] = None
-        return {"id": key, "status": "deleted"}
+        return {'id': key, 'status': 'deleted'}
     else:
-        return {"error": "key not found"}
+        return {}
 
 def get_kv(key):
     global in_memory_db
-    if key in in_memory_db and in_memory_db[key]:
+    if (key in in_memory_db) and in_memory_db[key]:
         return in_memory_db[key]
     else:
         return {"error": "key not found"}
 
 
 def list_kv():
-    result = []
-    for key, val in in_memory_db.items():
-        if val:
-            result.append(val)
+    result = [val for key,val in in_memory_db.items() if val]
     return result
 
 def filter_kv(filter):
@@ -67,30 +65,47 @@ class StudentsView(HTTPMethodView):
 
     # return list of all students
     async def get(self, request):
-        print(request.url)
         print(request.query_string)
         print(request.args)
-        print(raw.args)        
+        #print(raw.args)        
         return json(list_kv())
 
     # create a new student
     async def post(self, request):
         content = request.json
         content["id"] = generate_uuid()
-        return(json(create_kv(content["id"], content)))
+        headers = {"location": "student/"+content["id"]}
+        return(json(create_kv(content["id"], content),
+                              headers=headers,
+                              status=201))
 
 class SingleStudentView(HTTPMethodView):
 
     # return single student
     async def get(self, request, id):
-        return json(get_kv(id))
+        data = get_kv(id)
+        if 'error' in data.keys():
+            status = 404
+        else:
+            status = 200
+        return json(data, status=status)
 
     # update details of a student
     async def put(self, request, id):
         content = request.json
         content["id"] = id
+        data = update_kv(id, content)
+        if 'error' in data.keys():
+            status = 404
+        else:
+            status = 200
         return json(update_kv(id, content))
 
     # delete a student
     async def delete(self, request, id):
-        return json(delete_kv(id))
+        message = delete_kv(id)
+        if message:
+            status = 200
+        else:
+            status = 204
+        return json(message, status=status)
