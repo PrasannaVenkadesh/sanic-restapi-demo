470e48172debb8ec70e5650364dab9e6e268ba70
Suresh Kumar <sukumar.ext@orange.com>
2017-12-08 06:06:06 +0100

    students stuff added

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from sanic import Sanic
 from views.student import StudentsView, SingleStudentView
-from views.department import DepartmentsView, SingleDepartmentView
 
 app = Sanic()
 
@@ -8,10 +7,6 @@ if __name__ == "__main__":
 
     # API routing for students
     app.add_route(StudentsView.as_view(), '/students')
-    app.add_route(SingleStudentView.as_view(), '/student/<name>')
+    app.add_route(SingleStudentView.as_view(), '/students/<id>')
 
-    # API routing for departments
-    app.add_route(DepartmentsView.as_view(), '/departments')
-    app.add_route(SingleDepartmentView.as_view(), '/department/<name>')
- 
     app.run(host='0.0.0.0', port=8000)
